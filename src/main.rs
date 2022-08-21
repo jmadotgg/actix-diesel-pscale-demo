6be9beded6aa6994af295ef09be757f36fbed406
Julius Makel - I20 <julius.makel@nordakademie.de>
2022-08-21 16:30:29 +0200

    Move HOSTNAME and PORT to .env

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
 use actix_diesel_pscale_demo::routes;
 use actix_web::{web, App, HttpServer};
+use dotenv::dotenv;
 use routes::org::api_config;
-
-const HOSTNAME: &str = "localhost";
-const PORT: u16 = 8080;
+use std::env;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+    let addr = (
+        (&env::var("HOSTNAME").expect("HOSTNAME not set")[..]),
+        env::var("PORT")
+            .expect("PORT not set")
+            .parse::<u16>()
+            .expect("Invalid port"),
+    );
     HttpServer::new(move || App::new().service(web::scope("api").configure(api_config)))
-        .bind((HOSTNAME, PORT))?
+        .bind(addr)?
         .run()
         .await
 }
